0339d55d3c780be3ccf366dc0a20ad4166008cc9
PaulLition <paul.beck@lition.de>
2019-04-23 15:55:39 +0200

    added user registration validation

--- contracts/SynLoan.sol
+++ contracts/SynLoan.sol
@@ -10,11 +10,11 @@ creator: Marcel Jackisch
 */
 
 contract SynLoanData {
-    
+
     uint public loanId;     // A unique number that increments with every newly created loan
 
     LoanData[] public loans;
-    
+
     struct LoanData {
         uint id;                                // Loan ID
         string name;                            // Name of  the Loan
@@ -25,7 +25,7 @@ contract SynLoanData {
         mapping (address => uint) userToId;     // Gets local user id belonging (mapped to) an address in loan
         uint[] loanAmounts;                     // corresponding to participants
         bool[] approvalStatus;                  // Array to store approvals
-        address[] userList; 
+        address[] userList;
         uint8 numOfUsers;
     }
 
@@ -36,20 +36,20 @@ Struct user defines key data of participants such as banks and businesses
     struct userData {
         string name;
         string role;        // Borrower or Lender
-        address account;    
+        address account;
     }
 
     // Public array of all participants in dApp/Smart Contract (maybe unnecessary)
-    userData[] public users;      
-    
+    userData[] public users;
+
     // Dictionary to find account data
-    mapping (address => userData) addressToUserData; 
-    
+    mapping (address => userData) addressToUserData;
+
     // Necessary to require user can only added to a loan once
     mapping (address => mapping (uint => bool)) addressAssociated;
 
     // Map a loan id to an account address of registrating user
-    mapping (uint => address) loanToRegistrar; 
+    mapping (uint => address) loanToRegistrar;
 
     // counts the amount of loans belonging to the address
     mapping (address => uint) userLoanCount;
@@ -62,7 +62,7 @@ Struct user defines key data of participants such as banks and businesses
       require(msg.sender == loanToRegistrar[_loanId], "Only the owner of the loan has permission for this action");
       _;
     }
-    
+
     /*
     Modifier to check if sender is participant of a loan
     */
@@ -76,20 +76,20 @@ Struct user defines key data of participants such as banks and businesses
 
         loanToRegistrar[loanId] = msg.sender;   // Store the address of the user in mapping
         userLoanCount[msg.sender]++;            // necessary for array to count loans registered by user
-        
+
         // create LoanData instance in memory, later populate array
         LoanData memory ln;
-        ln.id = loanId;        
+        ln.id = loanId;
         ln.name = _name;
         ln.revisionNumber = 0;
         ln.registeringParty = msg.sender;
         ln.dataString = _dataString;
         ln.regTime = now;
-        
+
         loans.push(ln);
-        
+
         // Add loan creator himself/herself
-        addUserToLoan(loanId, msg.sender); 
+        addUserToLoan(loanId, msg.sender);
         loanId++; // Increment unique number
     }
 
@@ -98,7 +98,7 @@ Struct user defines key data of participants such as banks and businesses
     Update Loan Data, increment version / revision number
     Here, all the other data like loan amount, start date and other conditions shall be filled
     */
-    function updateLoan(uint _loanId, string memory _name, string memory _dataString, uint _loanAmount) 
+    function updateLoan(uint _loanId, string memory _name, string memory _dataString, uint _loanAmount)
         public onlyParticipant(_loanId)
     {
         loans[_loanId].name = _name;
@@ -114,7 +114,7 @@ Struct user defines key data of participants such as banks and businesses
 
     /*
     Functionality to delete loan
-    */   
+    */
     function deleteLoan(uint _id) public onlyRegistrar(_id) {
         delete loans[_id];
     }
@@ -150,57 +150,58 @@ Struct user defines key data of participants such as banks and businesses
         require(_account != address(0));
         require(addressAssociated[_account][_loanId] == false, "User already exists in loan");
         addressAssociated[_account][_loanId] = true;
-        
+
         userLoanCount[_account]++;
         uint userNum = loans[_loanId].numOfUsers++;
         // Adds user to mapping of loan (analog to incremented numOfUsers)
         loans[_loanId].userToId[_account] = userNum;
         // Pushes address to userList array (to retrieve all users, iterate)
         loans[_loanId].userList.push(_account);
-        
+
         // Let size of arrays that correspond with users grow in size
         loans[_loanId].approvalStatus.length++;
         loans[_loanId].loanAmounts.length++;
         return userNum;
     }
-    
-    
+
+
     /*
     Self-Registration of a user account
      */
     function selfRegistration (string memory _name, string memory _role) public {
 
         // Conditions to ensure no ghost users are registered
-        require(bytes (_name).length > 0, "Name must be specified"); 
-        
+        require(bytes (_name).length > 0, "Name must be specified");
+
         require(keccak256(abi.encode(_role)) == keccak256(abi.encode("lender")) || keccak256(abi.encode(_role)) == keccak256(abi.encode("borrower")), "Role must match 'lender' or 'borrower");
         require(bytes (addressToUserData[msg.sender].name).length == 0, "User has been registered before");
 
-        
-        // Consider if populating array is necessary 
+
+        // Consider if populating array is necessary
         userData memory u;
         u.name = _name;
         u.role = _role;
         u.account = msg.sender;
         users.push(u);
-    
+
         // Self-registration: Mapping ---- (-1??)
         addressToUserData[msg.sender] = u;
     }
 
     /*
-    Registration of a user account by _anyone_ (public) 
+    Registration of a user account by _anyone_ (public)
      */
     function userRegistration (string memory _name, string memory _role, address _account) public {
 
+
         // Conditions to ensure no ghost users are registered
-        require(bytes (_name).length > 0, "Name must be specified"); 
+        require(bytes (_name).length > 0, "Name must be specified");
 
         require(keccak256(abi.encode(_role)) == keccak256(abi.encode("lender")) || keccak256(abi.encode(_role)) == keccak256(abi.encode("borrower")), "Role must match 'lender' or 'borrower");
         require(bytes (addressToUserData[_account].name).length == 0, "User has been registered before");
 
 
-        // Consider if populating array is necessary 
+        // Consider if populating array is necessary
         userData memory u;
         u.name = _name;
         u.role = _role;
@@ -220,58 +221,58 @@ Struct user defines key data of participants such as banks and businesses
         addressToUserData[msg.sender].name = _name;
 
     }
-    
-    
+
+
     /*
     Helper function to retrieve [mapping userLoanCount]
-    */   
+    */
     function getUserLoanCount(address _addr) public view returns(uint) {
         return userLoanCount[_addr];
     }
-    
+
     /*
     Helper function to retrieve [mapping loanToRegistrar]
-    */   
+    */
     function getloanToRegistrar(uint _loanId) public view returns(address) {
         return loanToRegistrar[_loanId];
     }
-    
+
     /*
     Helper function to retrieve UserId from mapping inside struct
     */
     function getUserToId(uint256 _loanId, address _address) public view returns (uint256) {
         return loans[_loanId].userToId[_address];
     }
-    
+
     /*
     Helper function to retrieve data belonging to an address
-    */   
+    */
     function getUserDataByAddr(address _account) public view returns(userData memory) {
         return addressToUserData[_account];
     }
 
     /*
-    Helper function to retrieve List of all registered Addresses in Loan 
-    */   
+    Helper function to retrieve List of all registered Addresses in Loan
+    */
     function getUsersInLoan (uint256 _loanId) public view returns (address[] memory, uint) {
         address[] memory addrArr = loans[_loanId].userList;
         uint userCount = loans[_loanId].numOfUsers;
         return (addrArr, userCount);
     }
-        
+
     /*
     Helper function to retrieve approval status array
-    */   
+    */
     function getApprovalStatus(uint256 _loanId) public view returns (bool[] memory) {
-        bool[] memory array = loans[_loanId].approvalStatus; // approvalStatus is a bool array in a struct array 
+        bool[] memory array = loans[_loanId].approvalStatus; // approvalStatus is a bool array in a struct array
         return array;
     }
 
     /*
     Helper function to retrieve the loan amounts of the users from struct
-    */   
+    */
     function getLoanAmounts(uint256 _loanId) public view returns (uint[] memory) {
-        uint[] memory array = loans[_loanId].loanAmounts; 
+        uint[] memory array = loans[_loanId].loanAmounts;
         return array;
     }
 
@@ -295,7 +296,7 @@ Struct user defines key data of participants such as banks and businesses
     The function should return an array with all the loans the user is involved in, disregarding any other permissioning like read-write requests
     Important to log all the loans by the user
     As of now, only the registrar mapping is applied, a loan belonging to multiple users cannot be created yet
-    Info: mapping (uint => address) loanToRegistrar; 
+    Info: mapping (uint => address) loanToRegistrar;
     */
     function getLoansByUser(address _user) external view returns(uint[] memory) {
         // Create a new array with as many entries as Loans belong to the user
@@ -308,16 +309,16 @@ Struct user defines key data of participants such as banks and businesses
                 counter++;
             }
             // Check if user is part of the loan but not registrar
-            else { 
+            else {
                 address[] memory _userArr = loans[i].userList;
-                
+
                 for (uint c = 0; c < _userArr.length; c++ ) {
                     if (_userArr[c] == _user) {
                     result[counter] = i;
                     counter++;
                     }
                 }
-                
+
             }
         }
         return result;
